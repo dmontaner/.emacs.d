84fe43e42ad445fe758ce1d31d901c2b7333a729
dmontaner <david.montaner@gmail.com>
2018-08-21 15:03:53 +0100

    new

---
+++ github_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# update_github.sh
+# 2017-11-14 david.montaner@gmail.com
+# update all dependencies form github
+
+cd downloaded_from_github/
+
+for f in *
+do
+    echo $f
+    cd $f
+    # git branch  ## ALL ARE MASTER BRANCHES
+    git pull origin master
+    cd ..
+done
+
+echo "ess will need a make !!!!"

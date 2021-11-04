6a674f678906b98f0ca08280b99d52c25fcf4708
dmontaner <david.montaner@gmail.com>
2021-11-04 09:52:51 +0000

    diff colors

--- init.el
+++ init.el
@@ -697,3 +697,12 @@
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(ediff-diff-options "-w")
  '(ediff-split-window-function 'split-window-horizontally))
+
+(add-hook 'ediff-load-hook
+          (lambda ()
+            (set-face-foreground
+             ediff-current-diff-face-B "blue")
+            (set-face-background
+             ediff-current-diff-face-B "red")
+            (make-face-italic
+             ediff-current-diff-face-B)))

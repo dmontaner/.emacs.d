b8fb0a0ac7fbb9bd726a8bd87aa9274f584f6398
dmontaner <david.montaner@gmail.com>
2025-01-10 09:58:03 +0000

    cypher mode

--- init.el
+++ init.el
@@ -19,8 +19,8 @@
  '(ediff-even-diff-B ((t (:background "dark slate blue"))))
  '(ediff-odd-diff-A ((t (:background "dark blue"))))
  '(ediff-odd-diff-B ((t (:background "dark blue"))))
- '(mode-line-buffer-id ((t (:background unspecified :foreground unspecified))))
- '(ido-subdir ((t (:foreground unspecified)))))
+ '(ido-subdir ((t (:foreground unspecified))))
+ '(mode-line-buffer-id ((t (:background unspecified :foreground unspecified)))))
 
 
 ;; BASIC CUSTOMIZATION
@@ -257,7 +257,8 @@ or insert 2 double quotes and leave cursor in the middle."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dockerfile-mode lsp-mode load-env-vars gptel elpy)))
+ '(package-selected-packages
+   '(cypher-mode dockerfile-mode lsp-mode load-env-vars gptel elpy)))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -278,3 +279,7 @@ or insert 2 double quotes and leave cursor in the middle."
   (eshell)
   (insert "cypher-shell\n")  ;; Automatically run the command after EShell starts
   (eshell-send-input))
+
+(use-package cypher-mode
+  :ensure t
+  )

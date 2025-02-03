ba2fb6c4126a8c79b76164975184f0ca3d072efa
dmontaner <david.montaner@gmail.com>
2025-02-03 18:03:18 +0000

    minimal improved

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
-(setq inhibit-startup-message t)   ;; Hide the startup message
-(setq ring-bell-function 'ignore)  ;; Disable sound notifications
-(setq make-backup-files nil)       ;; Stop creating backup~ files
-(setq auto-save-default nil)       ;; Stop creating #autosave# files
-(defalias 'yes-or-no-p 'y-or-n-p)  ;; short yes no
-(setq-default indent-tabs-mode nil)  ;; use spaces instead of tabs when indenting
-(column-number-mode t)
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(setq ring-bell-function 'ignore)   ;; Disable sound notifications
+(setq make-backup-files nil)        ;; Stop creating backup~ files
+(setq auto-save-default nil)        ;; Stop creating #autosave# files
+(defalias 'yes-or-no-p 'y-or-n-p)   ;; short yes no
+(setq-default indent-tabs-mode nil) ;; use spaces instead of tabs when indenting
+(column-number-mode t)	            ;; include column number
+(menu-bar-mode -1)                  ;; disable menu bar (File, Edit, View...)
+(tool-bar-mode -1)                  ;; disable toolbar completely (Open, Save, Undo...)

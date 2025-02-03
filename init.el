6446641714c9a809257ddf2edae4f709d45d1a18
dmontaner <david.montaner@gmail.com>
2025-02-03 18:04:25 +0000

    formatted

--- init.el
+++ init.el
@@ -2,8 +2,8 @@
 (setq ring-bell-function 'ignore)   ;; Disable sound notifications
 (setq make-backup-files nil)        ;; Stop creating backup~ files
 (setq auto-save-default nil)        ;; Stop creating #autosave# files
-(defalias 'yes-or-no-p 'y-or-n-p)   ;; short yes no
 (setq-default indent-tabs-mode nil) ;; use spaces instead of tabs when indenting
+(defalias 'yes-or-no-p 'y-or-n-p)   ;; short yes no
 (column-number-mode t)	            ;; include column number
 (menu-bar-mode -1)                  ;; disable menu bar (File, Edit, View...)
 (tool-bar-mode -1)                  ;; disable toolbar completely (Open, Save, Undo...)
